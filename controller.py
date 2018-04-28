2980c07668d8774a218df53ab917560e44a87ee8
Matthew Hancox <m.hancox@ncl.ac.uk>
2018-04-28 14:20:53 +0100

    inital controller code

--- controller.py
+++ controller.py
@@ -1,13 +1,34 @@
 from microbit import *
+import radio
 
 radio.on()
-radio.config(group=45)
+radio.config(group=46)
 
-toSend = [0, 0]
+toSend = ""
 speed = 0
+direction = "straight"
 
 while True:
-    if button_a.was_pressed():
+    if accelerometer.get_x() < -200:
+        direction = "left"
+        
+        
+    if accelerometer.get_x() > 200:
+        direction = "right"
+     
+    if button_a.was_pressed():      
         speed = 255
-        toSend = [speed,speed]
-        radio.send(toSend)
+    
+    if button_b.was_pressed():
+        speed = 0
+    
+    if direction == "left":
+        toSend = str(0) + ',' + str(speed)
+    
+    if direction == "right":
+        toSend = str(speed) + ',' + str(0)
+        
+    radio.send("100,100")
+    
+    speed = 0
+    direction = "straight"
