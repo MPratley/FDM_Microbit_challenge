9c7d4a619f6e7290f849e69f35db20a9f1954c9f
Marcus Pratley <marcuspratley@gmail.com>
2018-04-28 13:52:30 +0100

    new car code

--- car.py
+++ car.py
@@ -1,34 +1,36 @@
 from microbit import *
 import radio
 
+leftWheelPWM = pin0
+rightWheelPWM = pin1
+
+leftWheelDirection = pin8
+rightWheelDirection = pin12
+
 def forwards():
-    pin12.write_digital(0)
-    pin8.write_digital(0)
+    rightWheelDirection.write_digital(0)
+    leftWheelDirection.write_digital(0)
     
 def backwards():
-    pin12.write_digital(1)
-    pin8.write_digital(1)
+    rightWheelDirection.write_digital(1)
+    leftWheelDirection.write_digital(1)
     
-def left():
-    pin0.write_analog(255)
-    pin1.write_analog(120)
+def turn_left():
+    leftWheelPWM.write_analog(255)
+    rightWheelPWM.write_analog(120)
     
-    
-def right():
-    pin0.write_analog(120)
-    pin1.write_analog(255)
+def turn_right():
+    leftWheelPWM.write_analog(120)
+    rightWheelPWM.write_analog(255)
+
+def 
 
 radio.on()
 radio.config(group=45)
 
-#radio.receive
-pin0.write_analog(255)
-pin1.write_analog(255)
-
 while True:
-    print("hello")
-    forwards()
-    right()
+    incoming_msg = radio.receive()
+    if incoming_msg:
+        leftWheelPWM.write_analog(incoming_msg[0])
+        rightWheelPWM.write_analog(incoming_msg[1])
     
-
-# Write your code here :-)
